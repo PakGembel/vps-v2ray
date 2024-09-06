5ff8642340f9a3e6900d272920aa15c84304d102
PakGembel <maz@hadipurwanto.com>
2024-09-06 10:17:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ clear
 apt-get update
 ##apt-get upgrade -y
 apt install nginx -y
-/etc/nginx/sites-available
-wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/PakGembel/vpn/main/nginx.conf
+rm -r /etc/nginx/sites-available/default
+wget -O /etc/nginx/sites-available/default https://raw.githubusercontent.com/PakGembel/vps-v2ray/main/default
 
 sudo apt install certbot python3-certbot-nginx
